df99f27ae862ff4a996347e3917ba3aa8a3f26f1
Jigs <jpatel22@jhu.edu>
2020-04-28 17:36:33 -0400

    update text

--- ALADIN/inc/hero.php
+++ ALADIN/inc/hero.php
@@ -5,7 +5,7 @@
                 <div class="col-12">
                     <div class="row align-items-center">
                         <div class="col-lg-12 text-center">
-                            <h1 data-aos="fade-up" data-aos-delay="100">Title 1</h1>
+                            <h1 data-aos="fade-up" data-aos-delay="100">Title 2</h1>
                             <p class="mb-4" data-aos="fade-up" data-aos-delay="200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsa nulla sed quis rerum amet natus quas necessitatibus.</p>
                             <p data-aos="fade-up" data-aos-delay="300"><a href="#" class="btn btn-primary py-3 px-5 btn-pill">BUTTON</a></p>
                         </div>
